af8beb00a850242037887bb5831a7b339c749b13
Vinicius Mendes <vinicius.henrique.92@hotmail.com>
2025-04-13 18:11:39 -0400

    feat: create active user id decorator

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -33,6 +33,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off'
     },
   },
 );
