58299e647b93e101a652c1b168c38d823c65b224
Vinicius Mendes <vinicius.henrique.92@hotmail.com>
2025-05-27 19:16:34 -0400

    feat: add loading state to button

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps } from 'react';
 import { cn } from '../../app/utils/cn';
+import { Spinner } from './Spinner';
 
 interface ButtonProps extends ComponentProps<'button'> {
   isLoading?: boolean;
@@ -9,6 +10,7 @@ export function Button({
   className,
   isLoading,
   disabled,
+  children,
   ...props
 }: ButtonProps) {
   return (
@@ -16,9 +18,11 @@ export function Button({
       {...props}
       disabled={disabled || isLoading}
       className={cn(
-        'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl font-medium text-white transition-all',
+        'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl font-medium text-white transition-all flex items-center justify-center',
         className,
       )}
-    />
+    >
+      {isLoading ? <Spinner className="w-6 h-6" /> : children}
+    </button>
   );
 }
