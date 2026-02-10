42d4008e76a307e3973ace5c36f11be0a4f51ca1
Vinicius Mendes <vinicius.henrique.92@hotmail.com>
2026-02-10 19:47:16 -0400

    feat: empty state accounts

--- frontend/src/view/Dashboard/components/Accounts/index.tsx
+++ frontend/src/view/Dashboard/components/Accounts/index.tsx
@@ -9,6 +9,7 @@ import { useAccountsController } from './useAccountsController';
 import { formatCurrency } from '../../../../app/utils/formatCurrency';
 import { cn } from '../../../../app/utils/cn';
 import { Spinner } from '../../../components/Spinner';
+import { PlusIcon } from '@radix-ui/react-icons';
 
 export function Accounts() {
   const {
@@ -18,6 +19,7 @@ export function Accounts() {
     areValuesVisible,
     toggleValuesVisibility,
     isLoading,
+    accounts,
   } = useAccountsController();
 
   return (
@@ -51,58 +53,78 @@ export function Accounts() {
           </div>
 
           <div className="flex-1 flex flex-col justify-end mt-10 md:mt-0">
-            <div>
-              <Swiper
-                spaceBetween={16}
-                slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
-                onSlideChange={(swiper) => {
-                  setSliderState({
-                    isBeginning: swiper.isBeginning,
-                    isEnd: swiper.isEnd,
-                  });
-                }}
-              >
-                <div
-                  className="flex items-center justify-between mb-4"
-                  slot="container-start"
-                >
+            {accounts.length === 0 && (
+              <>
+                <div className="mb-4" slot="container-start">
                   <strong className="text-white tracking-[-1px] text-lg">
                     Minhas contas
                   </strong>
-
-                  <SliderNavigation
-                    isBeginning={sliderState.isBeginning}
-                    isEnd={sliderState.isEnd}
-                  />
                 </div>
-                <SwiperSlide>
-                  <AccountCard
-                    color="#7950f2"
-                    name="Nubank"
-                    balance={1000.23}
-                    type="CHECKING"
-                  />
-                </SwiperSlide>
 
-                <SwiperSlide>
-                  <AccountCard
-                    color="#333"
-                    name="XP"
-                    balance={1000.23}
-                    type="INVESTMENT"
-                  />
-                </SwiperSlide>
+                <button className="mt-4 h-52 rounded-2xl border-2 border-dashed border-teal-600 flex flex-col items-center justify-center gap-4 text-white">
+                  <div className="w-11 h-11 rounded-full border-2 border-dashed border-white flex items-center justify-center">
+                    <PlusIcon className="w-6 h-6" />
+                  </div>
+                  <span className="tracking-[-0.5px] font-medium w-32 text-center">
+                    Cadastre uma nova conta
+                  </span>
+                </button>
+              </>
+            )}
+            {accounts.length > 0 && (
+              <div>
+                <Swiper
+                  spaceBetween={16}
+                  slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
+                  onSlideChange={(swiper) => {
+                    setSliderState({
+                      isBeginning: swiper.isBeginning,
+                      isEnd: swiper.isEnd,
+                    });
+                  }}
+                >
+                  <div
+                    className="flex items-center justify-between mb-4"
+                    slot="container-start"
+                  >
+                    <strong className="text-white tracking-[-1px] text-lg">
+                      Minhas contas
+                    </strong>
 
-                <SwiperSlide>
-                  <AccountCard
-                    color="#0f0"
-                    name="Carteira"
-                    balance={1000.23}
-                    type="CASH"
-                  />
-                </SwiperSlide>
-              </Swiper>
-            </div>
+                    <SliderNavigation
+                      isBeginning={sliderState.isBeginning}
+                      isEnd={sliderState.isEnd}
+                    />
+                  </div>
+                  <SwiperSlide>
+                    <AccountCard
+                      color="#7950f2"
+                      name="Nubank"
+                      balance={1000.23}
+                      type="CHECKING"
+                    />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <AccountCard
+                      color="#333"
+                      name="XP"
+                      balance={1000.23}
+                      type="INVESTMENT"
+                    />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <AccountCard
+                      color="#0f0"
+                      name="Carteira"
+                      balance={1000.23}
+                      type="CASH"
+                    />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+            )}
           </div>
         </>
       )}
--- frontend/src/view/Dashboard/components/Accounts/useAccountsController.tsx
+++ frontend/src/view/Dashboard/components/Accounts/useAccountsController.tsx
@@ -19,5 +19,6 @@ export function useAccountsController() {
     areValuesVisible,
     toggleValuesVisibility,
     isLoading: false,
+    accounts: [],
   };
 }
