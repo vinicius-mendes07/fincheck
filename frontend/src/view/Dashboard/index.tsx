737893dbf227135c31920d3d853a2513fb5bd77c
Vinicius Mendes <vinicius.henrique.92@hotmail.com>
2026-02-16 10:49:06 -0400

    feat: modal component

--- frontend/src/view/Dashboard/index.tsx
+++ frontend/src/view/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Logo } from '../components/Logo';
+import { Modal } from '../components/Modal';
 import { UserMenu } from '../components/UserMenu';
 import { Accounts } from './components/Accounts';
 import { DashboardProvider } from './components/DashboardContext';
@@ -24,6 +25,10 @@ export function Dashboard() {
         </main>
 
         <Fab />
+
+        <Modal open title="Receitas">
+          Conteudo
+        </Modal>
       </div>
     </DashboardProvider>
   );
